b22b500c69a667d7776bc3ccf7dfb0e5bae6764b
nampnh <nampnhgt00569@fpt.edu.vn>
2018-03-02 22:44:01 +0700

    Comment & Test

---
+++ UniversityDao/Dao/CommentDao.cs
@@ -0,0 +1,74 @@
+﻿using ModelPr.ModelViews;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversityDao.EF;
+
+namespace UniversityDao.Dao
+{
+    public class CommentDao
+    {
+        UniversityDbContext db = new UniversityDbContext();
+
+        public void Createtable()
+        {
+            db.Comments.Create();
+        }
+
+        public bool InsertComment(Comment model)
+        {
+            try
+            {
+                model.CreatedDate = DateTime.Now;
+                model.ModifiedDate = DateTime.Now;
+                db.Comments.Add(model);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public bool UpdateComment(Comment model)
+        {
+            try
+            {
+                var com = db.Comments.Find(model.CommentID);
+                com.ModifiedDate = DateTime.Now;
+                com.Description = model.Description;
+                com.Emotion = model.Emotion;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public bool DeleteComment(int id)
+        {
+            try
+            {
+                var com = db.Comments.Find(id);
+                db.Comments.Remove(com);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public List<Comment> CommentsbyIdea(int iid)
+        {
+            var result = (from a in db.Comments where a.IdeaID == iid select a).ToList();
+            return result;
+        }
+    }
+}
---
+++ UniversityDao/EF/Comment.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+
+namespace UniversityDao.EF
+{
+    [Table("Comment")]
+    public partial class Comment
+    {
+        [Key]
+        public int CommentID { get; set; }
+
+        public int IdeaID { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [StringLength(20)]
+        public string Emotion { get; set; }
+
+        public int? Category { get; set; }
+
+        public int? ViewCount { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+
+        [StringLength(250)]
+        public string CreatedBy { get; set; }
+
+        [StringLength(250)]
+        public string ModifiedBy { get; set; }
+
+        public DateTime? ClosedDate { get; set; }
+
+        public bool? Status { get; set; }
+    }
+}
